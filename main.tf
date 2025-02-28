d62c2354f31b6165bde566c8f77305ec38c88b3b
Andriy Kobetiak <andriy.k@mate.academy>
2025-03-01 00:27:43 +0200

    create-bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-# 1. Deploy an S3 storage bucket 
+# 1. Deploy an S3 storage bucket
+resource "aws_s3_bucket" "grafana_backups" {
+  bucket = "grafana-backups-andr1yk"
+  tags = {
+    Name = "grafana-backups"
+  }
+}
 
-# 2. Confugure bucket policy to allow grafana iam role to use storage 
+# 2. Confugure bucket policy to allow grafana iam role to use storage
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,1 @@
-grafana_iam_role_arn = ""
+grafana_iam_role_arn = "arn:aws:iam::337909773697:role/grafana"
