2bb70398cc18c44a49b67f8219f7880c3a833c1b
Andriy Kobetiak <andriy.k@mate.academy>
2025-03-01 00:38:13 +0200

    solution

--- main.tf
+++ main.tf
@@ -6,4 +6,34 @@ resource "aws_s3_bucket" "grafana_backups" {
   }
 }
 
-# 2. Confugure bucket policy to allow grafana iam role to use storage
+# 2. Configure bucket policy to allow grafana iam role to use storage
+data "aws_iam_policy_document" "grafana_backup_policy" {
+  statement {
+    sid       = "AllowListBucket"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.grafana_backups.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.grafana_iam_role_arn]
+    }
+  }
+
+  statement {
+    sid       = "AllowGetPutObjects"
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    resources = ["${aws_s3_bucket.grafana_backups.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.grafana_iam_role_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "grafana_backups_policy" {
+  bucket = aws_s3_bucket.grafana_backups.id
+  policy = data.aws_iam_policy_document.grafana_backup_policy.json
+}
